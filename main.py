35e227ef7fea93b8847eef75bf7896601d9adce7
wissemgrari <wissemgrari@outlook.com>
2024-02-22 23:52:50 +0100

    Add database integration and face encoding

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from recognition import FaceRecognition
+import db
+import face_encoder as fe
 
 if __name__ == '__main__':
+    # initialize the database connection
+    # connection = db.run_db()
+    # encode the faces
+    # fe.encode_faces(connection)
     fr = FaceRecognition()
     fr.run_recognition()
