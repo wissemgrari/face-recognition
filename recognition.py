9fcbeb91860f4d679230865c19506fa3f8ab0618
wissemgrari <wissemgrari@outlook.com>
2024-02-22 21:26:04 +0100

    Refactor face_confidence function and FaceRecognition class

--- recognition.py
+++ recognition.py
@@ -7,105 +7,107 @@ import math
 
 # Helper
 def face_confidence(face_distance, face_match_threshold=0.6):
-    range = (1.0 - face_match_threshold)
-    linear_val = (1.0 - face_distance) / (range * 2.0)
+	range = (1.0 - face_match_threshold)
+	linear_val = (1.0 - face_distance) / (range * 2.0)
 
-    if face_distance > face_match_threshold:
-        return str(round(linear_val * 100, 2)) + '%'
-    else:
-        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
-        return str(round(value, 2)) + '%'
+	if face_distance > face_match_threshold:
+		return str(round(linear_val * 100, 2)) + '%'
+	else:
+		value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
+		return str(round(value, 2)) + '%'
 
 
 class FaceRecognition:
-    face_locations = []
-    face_encodings = []
-    face_names = []
-    known_face_encodings = []
-    known_face_names = []
-    process_current_frame = True
+	face_locations = []
+	face_encodings = []
+	face_names = []
+	known_face_encodings = []
+	known_face_names = []
+	process_current_frame = True
 
-    def __init__(self):
-        self.encode_faces()
+	def __init__(self):
+		self.encode_faces()
 
-    def encode_faces(self):
-        for image in os.listdir('faces'):
-            face_image = face_recognition.load_image_file(f"faces/{image}")
-            face_encoding = face_recognition.face_encodings(face_image)[0]
+	def encode_faces(self):
+		for image in os.listdir('faces'):
+			face_image = face_recognition.load_image_file(f"faces/{image}")
+			face_encoding = face_recognition.face_encodings(face_image)[0]
 
-            self.known_face_encodings.append(face_encoding)
-            self.known_face_names.append(image.split('.')[0])
+			self.known_face_encodings.append(face_encoding)
+			self.known_face_names.append(image.split('.')[0])
 
-    def run_recognition(self):
-        video_capture = cv2.VideoCapture(0)
+	def run_recognition(self):
+		video_capture = cv2.VideoCapture(0)
 
-        if not video_capture.isOpened():
-            sys.exit('Video source not found...')
+		if not video_capture.isOpened():
+			sys.exit('Video source not found...')
 
-        while True:
-            ret, frame = video_capture.read()
+		while True:
+			ret, frame = video_capture.read()
 
-            # Only process every other frame of video to save time
-            if self.process_current_frame:
-                # Resize frame of video to 1/4 size for faster face recognition processing
-                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+			# Only process every other frame of video to save time
+			if self.process_current_frame:
+				# Resize frame of video to 1/4 size for faster face recognition processing
+				small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-                rgb_small_frame = small_frame[:, :, ::-1]
+				# Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+				rgb_small_frame = small_frame[:, :, ::-1]
 
-                # Find all the faces and face encodings in the current frame of video
-                self.face_locations = face_recognition.face_locations(rgb_small_frame)
-                self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+				# Find all the faces and face encodings in the current frame of video
+				self.face_locations = face_recognition.face_locations(rgb_small_frame)
+				self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
 
-                self.face_names = []
-                for face_encoding in self.face_encodings:
-                    # See if the face is a match for the known face(s)
-                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
-                    name = "Unknown"
-                    confidence = '???'
+				self.face_names = []
+				for face_encoding in self.face_encodings:
+					print(face_encoding)
+					print(self.known_face_encodings[0])
+					# See if the face is a match for the known face(s)
+					matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+					name = "Unknown"
+					confidence = '???'
 
-                    # Calculate the shortest distance to face
-                    face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+					# Calculate the shortest distance to face
+					face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
 
-                    best_match_index = np.argmin(face_distances)
-                    if matches[best_match_index]:
-                        name = self.known_face_names[best_match_index]
-                        confidence = face_confidence(face_distances[best_match_index])
+					best_match_index = np.argmin(face_distances)
+					if matches[best_match_index]:
+						name = self.known_face_names[best_match_index]
+						confidence = face_confidence(face_distances[best_match_index])
 
-                    self.face_names.append(f'{name} ({confidence})')
+					self.face_names.append(f'{name} ({confidence})')
 
-            self.process_current_frame = not self.process_current_frame
+			self.process_current_frame = not self.process_current_frame
 
-            # Display the results
-            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
-                # represents the top-left corner and he bottom-right corner of rectangle 
-                top *= 3
-                left *= 3
-                bottom *= 5
-                right *= 5
+			# Display the results
+			for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+				# represents the top-left corner and he bottom-right corner of rectangle 
+				top *= 3
+				left *= 3
+				bottom *= 5
+				right *= 5
 
-                # Create the frame with the name
-                if(confidence != '???' and float(confidence.split('%')[0]) > 80):   
-                  cv2.rectangle(frame, (left, top), (right, bottom), (0, 255 ,0), 2)
-                  cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255 ,0), cv2.FILLED)
-                  cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255 ,255), 1)
-                else:
-                  cv2.rectangle(frame, (left, top), (right, bottom), (0, 0 ,255), 2)
-                  cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0 ,255), cv2.FILLED)
-                  cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255 ,255), 1)
+				# Create the frame with the name
+				if(confidence != '???' and float(confidence.split('%')[0]) > 80):   
+				  cv2.rectangle(frame, (left, top), (right, bottom), (0, 255 ,0), 2)
+				  cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255 ,0), cv2.FILLED)
+				  cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255 ,255), 1)
+				else:
+				  cv2.rectangle(frame, (left, top), (right, bottom), (0, 0 ,255), 2)
+				  cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0 ,255), cv2.FILLED)
+				  cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255 ,255), 1)
 
-            # Display the resulting image
-            cv2.imshow('Face Recognition', frame)
+			# Display the resulting image
+			cv2.imshow('Face Recognition', frame)
 
-            # Hit 'q' on the keyboard to quit!
-            if cv2.waitKey(1) == ord('q'):
-                break
+			# Hit 'q' on the keyboard to quit!
+			if cv2.waitKey(1) == ord('q'):
+				break
 
-        # Release handle to the webcam
-        video_capture.release()
-        cv2.destroyAllWindows()
+		# Release handle to the webcam
+		video_capture.release()
+		cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    fr = FaceRecognition()
-    fr.run_recognition()
+	fr = FaceRecognition()
+	fr.run_recognition()
